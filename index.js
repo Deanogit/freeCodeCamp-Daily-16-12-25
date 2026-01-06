279e358be0128f66369cff9ee6c5c12a141f2792
Dean Howe <hideanhowe@gmail.com>
2026-01-06 13:34:26 +0000

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+// Consonant Count
+// Given a string and a target number, determine whether the string contains exactly the target number of consonants.
+
+// Consonants are all alphabetic characters except "a", "e", "i", "o", and "u" in any case.
+// Ignore digits, punctuation, spaces, and other non-letter characters when counting.
